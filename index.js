e5646b85254348e5810512702190ea18a54bf542
Shaxboz98 <sruziqulov@internet.ru>
2023-01-07 16:07:13 +0500

    secord commit

--- index.js
+++ index.js
@@ -366,3 +366,246 @@
 //   let counter2 = 2;
 // }
 // somemethod()
+
+
+//21.Temporal Dead Zone (Vaqtinchalik o'lik zona) nima
+
+// Vaqtinchalik o'lik zona - bu JavaScriptda o'zgaruvchini let va const kalit so'zlari 
+//bilan e'lon qilishda yuzaga keladigan xatti-harakatlar, lekin var bilan emas. ECMAScript 6 da 
+//letyoki consto'zgaruvchiga uning e'lon qilinishidan oldin (uning doirasi doirasida) kirish 
+//ReferenceErrorga olib keladi. Bu sodir bo'ladigan vaqt oralig'i, o'zgaruvchining bog'lanishi 
+//yaratilishi va uning e'lon qilinishi o'rtasidagi vaqt oralig'i vaqtinchalik o'lik zona deb ataladi.
+
+//  function somemethod() {
+//   console.log(counter1); // undefined
+//   console.log(counter2); // ReferenceError
+//   var counter1 = 1;
+//   let counter2 = 2;
+// }
+// somemethod()
+
+// 23.  JavaScriptda URL-manzilni qanday dekodlash yoki enkodlash mumkin?
+// encodeURI()funksiyasi URL manzilini kodlash uchun ishlatiladi. Bu funksiya parametr sifatida URL 
+//satrini talab qiladi va kodlangan qatorni qaytaradi. decodeURI()funksiya URL manzilini ochish uchun 
+//ishlatiladi. Bu funksiya parametr sifatida kodlangan URL satrini talab qiladi va bu dekodlangan qatorni 
+//qaytaradi.
+
+// Note: encodeURI()funksiyasi URL manzilini kodlash uchun ishlatiladi. Bu funksiya parametr sifatida URL 
+// satrini talab qiladi va kodlangan qatorni qaytaradi. decodeURI()funksiya URL manzilini ochish uchun 
+// ishlatiladi. Bu funksiya parametr sifatida kodlangan URL satrini talab qiladi va bu dekodlangan qatorni 
+// qaytaradi.
+
+// let uri = 'employeeDetails?name=john&occupation=manager';
+// let encoded_uri = encodeURI(uri);
+// let decoded_uri = decodeURI(encoded_uri);
+// console.log(decoded_uri);
+
+//24 Memoization nima
+// Memoizatsiya - bu avvaldan hisoblangan natijalarni keshlash orqali funktsiyaning ishlashini oshirishga 
+//harakat qiladigan dasturlash usuli. Har safar xotirada saqlangan funksiya chaqirilganda, uning
+// parametrlari keshni indekslash uchun ishlatiladi. Agar ma'lumotlar mavjud bo'lsa, uni butun funktsiyani
+// bajarmasdan qaytarish mumkin. Aks holda funktsiya bajariladi va natija keshga qo'shiladi. Keling, 
+//xotira bilan funksiya qo'shishga misol keltiraylik,
+
+// const memoizAddition = () => {
+//   let cache = {};
+//   return (value) => {
+//     if (value in cache) {
+//       console.log('Fetching from cache');
+//       return cache[value]; // Here, cache.value cannot be used as property name starts with the number which is not a valid JavaScript  identifier. Hence, can only be accessed using the square bracket notation.
+//     } else {
+//       console.log('Calculating result');
+//       let result = value + 20;
+//       cache[value] = result;
+//       return result;
+//     }
+//   };
+// };
+// // returned function from memoizAddition
+// const addition = memoizAddition();
+// console.log(addition(20)); //output: 40 calculated
+// console.log(addition(20)); //output: 40 cached
+
+//25.Hoisting nima
+// Yuk ko'tarish JavaScript mexanizmi bo'lib, unda kod bajarilishidan oldin o'zgaruvchilar va funksiya 
+//deklaratsiyasi o'z doirasining yuqori qismiga ko'chiriladi. Esda tutingki, JavaScript faqat 
+//deklaratsiyalarni ko'taradi, ishga tushirishni emas. Keling, o'zgaruvchan yuk ko'tarishning 
+//oddiy misolini olaylik,
+
+// console.log(message); //output : undefined
+// var message = 'The variable Has been hoisted';
+// // Yuqoridagi kod tarjimon uchun quyidagi kabi ko'rinadi,
+
+// var message;
+// console.log(message);
+// message = 'The variable Has been hoisted'
+
+//26. ES6 da qanday class(siniflar) mavjud
+// ES6 da Javascript sinflari JavaScript-ning mavjud prototipiga asoslangan merosiga nisbatan asosan 
+//sintaktik shakar hisoblanadi. Masalan, prototipga asoslangan meros quyidagi funksiya ifodasida yozilgan,
+
+// function Bike(model, color) {
+//   this.model = model;
+//   this.color = color;
+// }
+
+// Bike.prototype.getDetails = function () {
+//   return this.model + ' bike has' + this.color + ' color';
+// };
+// ES6 sinflari esa muqobil sifatida belgilanishi mumkin
+
+// class Bike {
+//   constructor(color, model) {
+//     this.color = color;
+//     this.model = model;
+//   }
+
+//   getDetails() {
+//     return this.model + ' bike has' + this.color + ' color';
+//   }
+// }
+
+//27
+// function Welcome(name) {
+//   var greetingInfo = function (message) {
+//     console.log(message + ' ' + name);
+//   };
+//   return greetingInfo;
+// }
+// var myFunction = Welcome('John');
+// myFunction('Welcome '); //Output: Welcome John
+// myFunction('Hello Mr.'); //output: Hello Mr.John
+
+//40 Javascriptda qamrov(scope) nima
+//Amal qilish doirasi - bu ish vaqtida kodingizning ma'lum bir qismidagi o'zgaruvchilar, funktsiyalar va objektlarga kirish imkoniyati. Boshqacha qilib aytganda, qamrov sizning kodingiz sohalarida o'zgaruvchilar va boshqa resurslarning ko'rinishini aniqlaydi.
+
+
+// 41 Veb-xotiraga(web storage) qanday kirish mumkin
+// Window obyekti mos ravishda (window.localStorage) va (window.sessionStorage) xossalariga ega bo'lgan WindowLocalStorageva WindowSessionStorageobjektlarini amalga oshiradi. Ushbu xususiyatlar Saqlash objektining namunasini yaratadi, bu orqali ma'lum domen va saqlash turi (sessiya yoki mahalliy) uchun ma'lumotlar elementlarini o'rnatish, olish va o'chirish mumkin. Masalan, siz mahalliy saqlash objektlarida quyida ko'rsatilganidek o'qishingiz va yozishingiz mumkinlocalStoragesessionStorage
+
+// 42localStorage.setItem('logo', document.getElementById('logo').value);
+// localStorage.getItem('logo');
+// 
+
+// 43 Session storageda qanday usullar mavjud
+// Window obyekti mos ravishda 
+//(window.localStorage) va (window.sessionStorage) 
+//xossalariga ega bo'lgan WindowLocalStorageva WindowSessionStorageobjektlarini 
+//amalga oshiradi. Ushbu xususiyatlar Saqlash objektining namunasini yaratadi, 
+//bu orqali ma'lum domen va saqlash turi (sessiya yoki mahalliy) uchun ma'lumotlar 
+//elementlarini o'rnatish, olish va o'chirish mumkin. Masalan, siz mahalliy saqlash 
+//objektlarida quyida ko'rsatilganidek o'qishingiz va yozishingiz mumkinlocalStoragesessionStorage
+
+// Save data to sessionStorage
+// sessionStorage.setItem('key', 'value');
+
+//  Get saved data from sessionStorage
+// let data = sessionStorage.getItem('key');
+
+// Remove saved data from sessionStorage
+// sessionStorage.removeItem('key');
+
+//Remove all saved data from sessionStorage
+// sessionStorage.clear();
+
+
+// 45 Saqlash hodisasi nima va uning voqea ishlovchisi (storage event and
+//   StorageEvent - bu boshqa hujjat kontekstida saqlash joyi o'zgartirilganda paydo bo'ladigan hodisa. Holbuki, saqlash xususiyati saqlash hodisalarini qayta ishlash uchun EventHandler hisoblanadi. Sintaksis quyidagi kabi bo'ladi
+  
+  // window.onstorage = functionRef;
+//   Keling, saqlash kaliti va uning qiymatlarini qayd qiluvchi xotira hodisasi ishlovchisidan foydalanish misolini olaylik
+  
+  // window.onstorage = function (e) {
+  //   console.log(
+  //     'The ' +
+  //       e.key +
+  //       ' key has been changed from ' +
+  //       e.oldValue +
+  //       ' to ' +
+  //       e.newValue +
+  //       '.'
+  //   );
+  // };
+//   
+  
+//   Nima uchun sizga veb-xotira kerak
+//   Veb-saqlash yanada xavfsizroq va katta hajmdagi ma'lumotlar veb-sayt ishlashiga ta'sir qilmasdan mahalliy sifatida saqlanishi mumkin. Bundan tashqari, ma'lumotlar hech qachon serverga o'tkazilmaydi. Shuning uchun bu Cookie-fayllarga qaraganda ko'proq tavsiya etilgan yondashuv.
+  
+//   ⬆ Back to Top
+  
+//   Veb-xotira brauzerining yordamini qanday tekshirish mumkin
+//   Veb-xotiradan foydalanishdan oldin brauzerning localStorage va sessionStorage uchun qo'llab-quvvatlanishini tekshirishingiz kerak,
+  
+//   ```javascript
+//   if (typeof Storage !== 'undefined') {
+//     // Code for localStorage/sessionStorage.
+//   } else {
+//     // Sorry! No Web Storage support..
+//   }
+//   ```
+  
+//   **[⬆ Back to Top](#table-of-contents)**
+//   Veb-workersning brauzer qo'llab-quvvatlashini qanday tekshirish
+//   Uni ishlatishdan oldin veb-xodimlar uchun brauzer yordamini tekshirishingiz kerak
+  
+//   if (typeof Worker !== 'undefined') {
+//     // code for Web worker support.
+//   } else {
+//     // Sorry! No Web Worker support..
+//   }
+//   ⬆ Back to Top
+  
+//   Veb-workerga misol keltiring
+//   Hisoblash misolida veb ishchilaridan foydalanishni boshlash uchun quyidagi bosqichlarni bajarishingiz kerak
+  
+//   Veb ishchi faylini yarating: Hisoblash qiymatini oshirish uchun skript yozishingiz kerak. Keling, uni counter.js deb nomlaymiz
+//   let i = 0;
+  
+//   function timedCount() {
+//     i = i + 1;
+//     postMessage(i);
+//     setTimeout('timedCount()', 500);
+//   }
+  
+//   timedCount();
+//   Bu yerda postMessage() usuli xabarni HTML sahifasiga qaytarish uchun ishlatiladi
+  
+//   Veb ishchi objektini yaratish: Brauzer qo'llab-quvvatlashini tekshirish orqali veb-ishchi objektini yaratishingiz mumkin. Keling, bu faylni web_worker_example.js deb nomlaymiz
+//   if (typeof w == 'undefined') {
+//     w = new Worker('counter.js');
+//   }
+//   va biz veb-ishchidan xabarlar olishimiz mumkin
+  
+//   w.onmessage = function (event) {
+//     document.getElementById('message').innerHTML = event.data;
+//   };
+//   Veb ishchini tugatish: Veb ishchilari xabarlarni tinglashda davom etadilar (hatto tashqi skript tugagandan keyin ham) u tugatilmaguncha. Siz xabarlarni tinglashni tugatish uchun terminate() usulidan foydalanishingiz mumkin.
+//   w.terminate();
+//   Veb ishchisini qayta ishlatish: Agar ishchi o'zgaruvchini aniqlanmagan qilib qo'ysangiz, kodni qayta ishlatishingiz mumkin
+//   w = undefined;
+//   ⬆ Back to Top
+  
+//   Web-ishchilarning DOM-da qanday cheklovlari bor
+//   WebWorkers quyida joylashgan JavaScript obyektlariga kirish huquqiga ega emas, chunki ular tashqi fayllarda aniqlangan
+  
+//   1. Window object
+//   2. Document object
+//   3. Parent object
+  
+//   
+
+
+// 'use strict';
+//  x = 3.14;
+// console.log(x);
+
+//74
+// let count=null;
+// console.log(count+1);
+
+
+// 75 Eval nima
+// eval() funktsiyasi satr sifatida taqdim etilgan JavaScript kodini baholaydi.
+//  Satr JavaScript ifodasi, o'zgaruvchi, bayonot yoki bayonotlar ketma-ketligi bo'lishi mumkin.
+// console.log(eval('1 + 2')); 
+
